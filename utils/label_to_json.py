20b2cf98a6fa4b61508cddf6f350163d8f121205
Himanshu Vishwakarma <himvish997@gmail.com>
2019-10-24 17:20:41 +0530

    update the label.json file name

--- utils/label_to_json.py
+++ utils/label_to_json.py
@@ -3,7 +3,6 @@ import json
 label_class_file = '../data/label.txt'
 
 label_class = list()
-
 counter = 1
 with open(label_class_file, 'r') as f:
     for cls_name in f:
@@ -15,7 +14,7 @@ with open(label_class_file, 'r') as f:
 json_data = json.dumps(label_class, indent=4)
 print(json_data)
 
-with open('../data/labels.json', 'w') as f:
+with open('../data/label.json', 'w') as f:
     json.dump(label_class, f, indent=4)
 
 
